a62afb08fed24b20029a79491a76d1aecf517250
Jamie Scott <jscott1277@hotmail.com>
2017-08-15 13:47:18 -0500

    made sure ignored file gets in

---
+++ src/app/config.js
@@ -0,0 +1,17 @@
+/**
+ * Copy this and save as 'config.js'
+ * - Edit the params as you see fit.
+ */
+
+/**
+ * Bot configuration
+ * @type {{}}
+ */
+var config = {
+    // Your API token from: https://discordapp.com/developers/applications/me
+    // APP BOT USER > Token:
+    botToken: 'MzQ3MDQ2MzA2NTI4NzU1NzE0.DHSsaw.09q1BqcL_u2-8sWMtAQdyel863E',
+};
+
+// Export it
+module.exports = config;
