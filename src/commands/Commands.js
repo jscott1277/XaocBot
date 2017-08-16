04d21dfac8faf3b5371c021e9d50a9c3f120168c
Jamie Scott <jscott1277@hotmail.com>
2017-08-16 09:50:56 -0500

    Changed cmd to xaoc

--- src/commands/Commands.js
+++ src/commands/Commands.js
@@ -1,8 +1,8 @@
 module.exports = Object.freeze({
-    GLOBAL_HELLO: 'hello mognet',
-    GLOBAL_HI: 'hi mognet',
+    GLOBAL_HELLO: 'hello xaocbot',
+    GLOBAL_HI: 'hi xaocbot',
 
-    COMMAND_KEY: '!mog',
+    COMMAND_KEY: '!xaoc',
     COMMAND_XIVDB_SEARCH: 'find',
     COMMAND_XIVDB_GET_ITEM: 'item', // chains onto find
     COMMAND_XIVDB_GET_QUEST: 'quest',
