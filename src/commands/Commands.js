602c0b4760d318620e6b104d134fa428ef8d724c
Jamie Scott <jscott1277@hotmail.com>
2017-08-16 09:54:03 -0500

    Revert "Changed cmd to xaoc"
    This reverts commit 04d21dfac8faf3b5371c021e9d50a9c3f120168c.

--- src/commands/Commands.js
+++ src/commands/Commands.js
@@ -1,8 +1,8 @@
 module.exports = Object.freeze({
-    GLOBAL_HELLO: 'hello xaocbot',
-    GLOBAL_HI: 'hi xaocbot',
+    GLOBAL_HELLO: 'hello mognet',
+    GLOBAL_HI: 'hi mognet',
 
-    COMMAND_KEY: '!xaoc',
+    COMMAND_KEY: '!mog',
     COMMAND_XIVDB_SEARCH: 'find',
     COMMAND_XIVDB_GET_ITEM: 'item', // chains onto find
     COMMAND_XIVDB_GET_QUEST: 'quest',
