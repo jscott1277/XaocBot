909deeb8b014d3a6b33bfca00f0d973cf03561ea
Jamie Scott <jscott1277@hotmail.com>
2017-08-18 14:53:06 -0600

    Fixed numbering

--- src/commands/HandlerXIVDB.js
+++ src/commands/HandlerXIVDB.js
@@ -37,7 +37,7 @@ class HandlerXIVDB {
                     if (i < 4) {
                         let content = result.results[i];
                         response.push(Language.say('XIVDB_RESULTS_CONTENT', [
-                            i,
+                            (i + 1),
                             content.id,
                             content.name,
                             content.url_xivdb
