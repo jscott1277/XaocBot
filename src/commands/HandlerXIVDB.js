96183a6f96ba05f71be0c7c2d4e79652db2a34b1
Jamie Scott <jscott1277@hotmail.com>
2017-08-18 14:03:30 -0600

    Added no results found

--- src/commands/HandlerXIVDB.js
+++ src/commands/HandlerXIVDB.js
@@ -23,6 +23,7 @@ class HandlerXIVDB {
 
                 // skip if no results
                 if (result.total == 0) {
+                    response.push(Language.say('XIVDB_RESULTS_CONTENT_FOUND_NONE', [ string ]));
                     continue;
                 }
 
